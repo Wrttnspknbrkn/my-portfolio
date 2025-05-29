e831890b7d6ca4ecafdf170cfc0fa953b6f042bd
Wrttnspknbrkn <kchampfameyeh@gmail.com>
2025-05-29 20:04:14 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { collection, addDoc, getDocs } from "@firebase/firestore"; 
 
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB-lfUt1adpQ0KYcFFW_oAWTJVfHDOOZy8",
-//   authDomain: "portofolio-web-3e8e8.firebaseapp.com",
-//   projectId: "portofolio-web-3e8e8",
-//   storageBucket: "portofolio-web-3e8e8.appspot.com",
-//   messagingSenderId: "25195509306",
-//   appId: "1:25195509306:web:2b635dcf997137bf612703"
-// };
-
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD77Qujrk8qR3MLTuOAfMp_eGXqGWITZHs",
   authDomain: "my-portfolio-7071.firebaseapp.com",
